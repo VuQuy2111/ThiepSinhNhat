497e8b21c6b9f422c0b2885ebd9e210e426ae2ef
VuQuy2111 <152176154+VuQuy2111@users.noreply.github.com>
2023-11-27 07:30:08 -0800

    Add files via upload
    Create

---
+++ config.js
@@ -0,0 +1,5 @@
+const nameGirl = 'Em';
+const giftUrl = 'https://www.facebook.com/mi.le.18294';
+const eventName = 'Chúc Mừng Sinh Nhật Chị Mi 28-11';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Chúc chị yêu sinh nhật vui vẻ, tuổi mới mãi xinh gái,nhiều tiền và hạnh phúc bên gia đình nhỏ của mình.Happy Birthday Make A Wish. Dear sister, hope you receive everything you wish for this year. You deserve the best! Thanks for being such a great sister.💘';
